024c97e44b53f09b0f18cfd30c6e680200400463
DESKTOP-INST8GU\magic <magicghost.vu@gmail.com>
2018-05-05 18:01:05 +0700

    #add choose file action and load bitmap (not done)

--- project-android/app/src/main/java/activities/HomeActivity.java
+++ project-android/app/src/main/java/activities/HomeActivity.java
@@ -1,19 +1,25 @@
 package activities;
 
 
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.m.uet.apptranslate.R;
 
+import java.io.File;
+
+import constants.Constants;
 import orc.man.OrcManager;
+import utils.FileUtils;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -21,6 +27,13 @@ public class HomeActivity extends AppCompatActivity {
     private ImageButton button_setting;
     private ImageButton button_saved;
     private ImageButton camera;
+
+
+    private final int requestCodeChooseFile = 1001;
+
+
+    String tag = "Home_Tag";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +46,7 @@ public class HomeActivity extends AppCompatActivity {
         button_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,SettingActivity.class);
+                Intent intent = new Intent(HomeActivity.this, SettingActivity.class);
                 startActivity(intent);
 
             }
@@ -43,14 +56,77 @@ public class HomeActivity extends AppCompatActivity {
         button_saved.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this,SavedActivity.class);
+                Intent intent = new Intent(HomeActivity.this, SavedActivity.class);
                 startActivity(intent);
 
             }
         });
 
 
+        Constants.intitConstant();
+
         OrcManager.getInstance().init();
 
+    }
+
+
+    // gắn hàm này vào nút chọn file
+    private void showChooseFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    requestCodeChooseFile);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        switch (requestCode) {
+            case requestCodeChooseFile: {
+
+                Uri uri = data.getData();
+
+                Log.d(tag, "uri file is " + uri.toString());
+
+                try {
+                    String path = FileUtils.getPath(this, uri);
+                    Log.d(tag, "File Path: " + path);
+
+                    File file = new File(path);
+
+                    //String fileName= file.getName();
+
+                    String extFile = FileUtils.getExtensionFile(file);
+
+                    if (!Constants.getExtAllow().contains(extFile)) {
+                        Toast.makeText(this, "File not supported", Toast.LENGTH_SHORT);
+                    } else {
+                        //todo: load bit map ở đây
+
+                        
+
+                    }
+
+
+                } catch (Exception e) {
+
+                }
+
+                // load lên một bit map để xử lý tiếp setView vvv....
+                break;
+            }
+        }
     }
 }
--- project-android/app/src/main/java/constants/Constants.java
+++ project-android/app/src/main/java/constants/Constants.java
@@ -1,5 +1,8 @@
 package constants;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by magic on 05/05/2018.
  */
@@ -8,4 +11,15 @@ public class Constants {
     public static final String TAG = "appTrans";
 
 
+    private static Set<String> extAllow;
+
+    public static void intitConstant() {
+        extAllow = new HashSet<>();
+        extAllow.add("jpg");
+        extAllow.add("png");
+    }
+
+    public static Set<String> getExtAllow() {
+        return extAllow;
+    }
 }
