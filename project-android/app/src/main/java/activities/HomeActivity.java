04c4f863aa338438397836b68056bdd0ee5dc4e7
DESKTOP-3477J35\PC <thanhcriss@gmail.com>
2018-05-13 18:02:11 +0700

    #add spinner

--- project-android/app/src/main/java/activities/HomeActivity.java
+++ project-android/app/src/main/java/activities/HomeActivity.java
@@ -14,14 +14,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.m.uet.apptranslate.R;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import constants.Constants;
 import mytask.GetTextFromBitMap;
@@ -36,6 +40,8 @@ public class HomeActivity extends AppCompatActivity {
     private ImageButton button_image;
     ImageView iv;
     TextView text_View;
+
+    Spinner spinner;
 
     private final int requestCodeChooseFile = 6384;
 
@@ -75,6 +81,26 @@ public class HomeActivity extends AppCompatActivity {
         Constants.initConstant();
 
         OrcManager.getInstance().init();
+
+        //todo: spinner
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.country_name,android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         //todo: add permission here
 
