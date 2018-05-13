12d88f196a8a60a84880272910fdba6004025d68
DESKTOP-3477J35\PC <thanhcriss@gmail.com>
2018-05-14 06:36:02 +0700

    #add setting layout

--- project-android/app/src/main/java/activities/SettingActivity.java
+++ project-android/app/src/main/java/activities/SettingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.m.uet.apptranslate.R;
@@ -14,15 +15,25 @@ public class SettingActivity extends AppCompatActivity {
 
     private ImageButton button_home2;
     private ImageButton button_translate2;
+    private Button button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        button = findViewById(R.id.button_about);
+        button.setOnClickListener(v ->  {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingActivity.this,SupportActivity.class));
+            }
+        });
+
+
 
 
         button_home2 = findViewById(R.id.button_home2);
-        button_home2.setOnClickListener(new View.OnClickListener() {
+        button_home2.setOnClickListener(v ->  {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SettingActivity.this,HomeActivity.class);
@@ -31,7 +42,7 @@ public class SettingActivity extends AppCompatActivity {
         });
 
         button_translate2 = findViewById(R.id.button_translate2);
-        button_translate2.setOnClickListener(new View.OnClickListener() {
+        button_translate2.setOnClickListener(v ->  {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SettingActivity.this,SavedActivity.class);
--- project-android/app/src/main/java/activities/SupportActivity.java
+++ project-android/app/src/main/java/activities/SupportActivity.java
@@ -2,6 +2,7 @@ package activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import com.m.uet.apptranslate.R;
 
@@ -11,5 +12,11 @@ public class SupportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width*.8),(int)(height*.6));
+        getSupportActionBar().hide();
     }
 }
